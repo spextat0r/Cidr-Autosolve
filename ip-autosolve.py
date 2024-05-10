2bd5be7dcc135ca9d7c0ca1a4eb7f88c72537bde
Daniel <57763139+samiam1086@users.noreply.github.com>
2024-05-10 10:58:17 -0400

    Update ip-autosolve.py

--- ip-autosolve.py
+++ ip-autosolve.py
@@ -2,6 +2,9 @@ import ipaddress
 import argparse
 import sys, os
 
+def pub_or_priv(ipaddress_to_check):
+    return "Private" if (ipaddress.ip_address(ipaddress_to_check).is_private) else "Public"
+
 def parse_hosts_file(hosts_file):  # parse our host file
     hosts = []
     if os.path.isfile(hosts_file): # ensure the file exists otherwise try it as if they passed an ip or cidr to the command line
@@ -47,32 +50,48 @@ if __name__ == '__main__':
     parser.add_argument("scope_file", help="Path to a file containing the full scope can be 1 ip per line or 1 cidr per line")
     parser.add_argument("exclusions_file", help="Path to a file containing the list of exclusions can be 1 ip per line or 1 cidr per line")
     parser.add_argument('-o', default='cidr_clean.txt', help='Outputfile name Default=cidr_clean.txt')
+    parser.add_argument('-t', default='cidr', choices=['ips', 'cidr'], help='Type of output you want Cidr notation or just raw IPs Options=[ips,cidr]')
 
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(1)
 
-    args = parser.parse_args()
+    options = parser.parse_args()
 
-    scope = parse_hosts_file(args.scope_file)
+    scope = parse_hosts_file(options.scope_file)
     print('Scope contains {} ips'.format(str(len(scope))))
-    exclusions = parse_hosts_file(args.exclusions_file)
+    exclusions = parse_hosts_file(options.exclusions_file)
     print('Exclusions contains {} ips'.format(str(len(exclusions))))
 
     scope_exclusions_removed = []
 
-    for ip in scope:
+    for ip in scope: # remove invalid ips
         if ip not in exclusions:
             try:
                 test = ipaddress.ip_address(ip)
                 scope_exclusions_removed.append(ip)
             except ValueError:
-                print('address/netmask is invalid: %s' % ip)
+                print('Invalid IP address detected skipping: %s' % ip)
                 continue
 
-    print('Cleaned list contains {} ips'.format(str(len(scope_exclusions_removed))))
-    scope_exclusions_removed = sorted(scope_exclusions_removed, key=ipaddress.IPv4Address)
-    with open(args.o, 'a') as f:
+    scope_exclusions_removed = sorted(scope_exclusions_removed, key=ipaddress.IPv4Address)# sort the list of ips
+    private = False
+    public = False
+    for ip in scope_exclusions_removed: # check if there are public and private ips in the scope
+        if pub_or_priv(ip) == 'Private':
+            private = True
+        else:
+            public = True
+
+    if public and private:
+        print('WARNING: Your scope contains both public and private IP addresses')
+
+    if options.t == 'cidr':
+        scope_exclusions_removed = [ipaddress.IPv4Address(line) for line in scope_exclusions_removed] # convert the list of strings to a list of ipv4address objects
+        scope_exclusions_removed = [ip.with_prefixlen for ip in ipaddress.collapse_addresses(scope_exclusions_removed)] # convert that to cidr
+
+    print('Cleaned list contains {} ips/subnets'.format(str(len(scope_exclusions_removed))))
+    with open(options.o, 'a') as f:
         for item in scope_exclusions_removed:
             f.write(item + '\n')
     f.close()
